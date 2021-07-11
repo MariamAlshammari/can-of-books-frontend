af5e25265c34ed53c37282ce1e0987e721b536c1
mariamalshammari <malshammari37@gmail.com>
2021-07-11 17:22:03 +0300

    lab11

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
 import React from 'react';
 import Header from './Header';
-import IsLoadingAndError from './IsLoadingAndError';
+// import IsLoadingAndError from './IsLoadingAndError';
 import Footer from './Footer';
+import { withAuth0 } from '@auth0/auth0-react';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
+import Login from './Login';
+import BestBooks from './BestBooks'
+import Profile from './components/Profile';
 
 class App extends React.Component {
 
   render() {
+    
     console.log('app', this.props);
-    return(
+    return (
       <>
         <Router>
-          <IsLoadingAndError>
-            <Header />
-            <Switch>
-              <Route exact path="/">
-                {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
-              </Route>
-              {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
-            </Switch>
-            <Footer />
-          </IsLoadingAndError>
+          {/* <IsLoadingAndError> */}
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+
+              {this.props.auth0.isAuthenticated ?
+                <BestBooks />
+                :
+                <Login />}
+
+              
+            </Route>
+            <Route exact path="/profile">
+              {this.props.auth0.isAuthenticated && <Profile /> }
+            </Route>
+            {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+          </Switch>
+          <Footer />
+          {/* </IsLoadingAndError> */}
         </Router>
       </>
     );
+   
+    
   }
 }
 
-export default App;
+export default withAuth0(App);
