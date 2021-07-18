cd0210770145c2d2133c8d649a73c73d4f3352e7
mariamalshammari <malshammari37@gmail.com>
2021-07-18 03:46:51 +0300

    lab12fronDone

--- src/App.js
+++ src/App.js
@@ -15,37 +15,6 @@ import Profile from './components/Profile';
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      bookArr: [],
-      showBooksComponent: false,
-      userEmail:''
-      
-    }
-  }
-  componentDidMount= async () => {
-    try {
-    const { user }=this.props.auth0;
-
-    await this.setState({
-      userEmail: `${user.email}`})
-  
-      
-      
-        
-        let url=`${process.env.REACT_APP_PORT}books?userEmail=malshammari37@gmail.com`;
-
-      let result = await axios.get(url);
-      
-      await this.setState({
-        bookArr: result.data,
-        showBooksComponent: true
-      });
-
-    } catch (error) {
-      console.log(error);
-    }}
   
   render() {
     
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,18 +2,63 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
+import { withAuth0 } from '@auth0/auth0-react';
+import axios from 'axios'
+import Mycard from './components/Mycard';
+// import Card from 'react-bootstrap/Card'
 
 class MyFavoriteBooks extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookArr: [],
+      showBooksComponent: false,
+      userEmail:''
+      
+    }
+  }
+  componentDidMount= async () => {
+    try {
+    const { user }=this.props.auth0;
+
+    await this.setState({
+      userEmail: `${user.email}`})
+  
+      
+          //http://localhost:3005/books?userEmail=malshammari37@gmail.com
+
+        
+        let url=`${process.env.REACT_APP_PORT}/books?userEmail=${this.state.userEmail}`;
+
+      let result = await axios.get(url);
+      
+      await this.setState({
+        bookArr: result.data,
+        showBooksComponent: true
+      });
+
+    } catch (error) {
+      console.log(error);
+    }}
+
   render() {
     return(
+      
       <Jumbotron>
+        <>
         <h1>My Favorite Books</h1>
         <p>
           This is a collection of my favorite books
         </p>
+
+        <Mycard bookArr={this.state.bookArr} showBooksComponent={this.state.showBooksComponent}/>
+        </> 
       </Jumbotron>
+    
+    
     )
   }
 }
 
-export default MyFavoriteBooks;
+export default withAuth0(MyFavoriteBooks);
